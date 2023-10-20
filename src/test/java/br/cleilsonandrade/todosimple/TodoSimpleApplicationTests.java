af10f83890d4f05095e656e468ef8d66df754bb8
Cleilson-Andrade <cleilson.andrade@anjunexpress.com.br>
2023-10-20 09:13:24 -0300

    first commit

---
+++ src/test/java/br/cleilsonandrade/todosimple/TodoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cleilsonandrade.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
